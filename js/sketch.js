08cc473507460a2408103bb478d1363f4695d1d4
Luke Prescott <luke.prescott@presport.us>
2019-05-08 12:10:56 -0400

    Setup framerate, and canvas

--- js/sketch.js
+++ js/sketch.js
@@ -1,7 +1,14 @@
 // The statements in the setup() function
 // execute once when the program begins
 function setup() {
+    
+    //Specifies the number of frames to be displayed every second. https://p5js.org/reference/#/p5/frameRate
+    frameRate(1);
 
+    //Creates a canvas element in the document, and sets the dimensions of it in pixels. https://p5js.org/reference/#/p5/createCanvas
+    canvas = createCanvas(innerWidth, innerHeight);
+    canvas.position(0,0);
+    canvas.style('z-index', '-1');
 }
 
 // The statements in draw() are executed until the
