919e6d1f4b924c6f7f7b0ca1e9b8ce7e8511ad09
Luke Prescott <luke.prescott@presport.us>
2019-05-08 12:30:03 -0400

    Initialized sizeofcell, numCol, numRow
    Calculated size of columns and rows based on page size

--- js/sketch.js
+++ js/sketch.js
@@ -1,14 +1,26 @@
+// Global variables with values
+// The length of one square side
+var cellSize = 40;
+
+// Global variables without values
+var numCol;
+var numRow;
+
 // The statements in the setup() function
 // execute once when the program begins
 function setup() {
     
-    //Specifies the number of frames to be displayed every second. https://p5js.org/reference/#/p5/frameRate
+    // Specifies the number of frames to be displayed every second. https://p5js.org/reference/#/p5/frameRate
     frameRate(1);
 
-    //Creates a canvas element in the document, and sets the dimensions of it in pixels. https://p5js.org/reference/#/p5/createCanvas
+    // Creates a canvas element in the document, and sets the dimensions of it in pixels. https://p5js.org/reference/#/p5/createCanvas
     canvas = createCanvas(innerWidth, innerHeight);
     canvas.position(0,0);
     canvas.style('z-index', '-1');
+
+    // Find the number of cols and rows, and round
+    numCol = round(innerWidth/cellSize);
+    numRow = round(innerHeight/cellSize);
 }
 
 // The statements in draw() are executed until the
