9514dd9eda8e38d766527036039eba095ea58062
Luke Prescott <luke.prescott@presport.us>
2019-05-08 12:05:55 -0400

    Initalized Sketch.js

---
+++ js/sketch.js
@@ -0,0 +1,13 @@
+// The statements in the setup() function
+// execute once when the program begins
+function setup() {
+
+}
+
+// The statements in draw() are executed until the
+// program is stopped. Each statement is executed in
+// sequence and after the last line is read, the first
+// line is executed again.
+function draw() {
+    
+}
